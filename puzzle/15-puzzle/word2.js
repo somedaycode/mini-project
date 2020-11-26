5473eee77cc055ecd81e0132c13a44019f9f05db
SEONKYU\difke <somedaycode@gmail.com>
2020-11-27 04:29:00 +0900

    add timer in puzzle game

--- puzzle/15-puzzle/word2.js
+++ puzzle/15-puzzle/word2.js
@@ -3,13 +3,18 @@ let str = document.querySelector('#word1').textContent;
 let word1 = document.querySelector('#word1');
 const word2 = document.querySelector('#word2');
 const correct = document.querySelector('.correct');
+const progress = document.querySelector('.time');
 
 //game object
 const game = {
+  // startTime을 지정함으로서 시작시간 저장
+  startTime: new Date(),
+  clock: setInterval(currentTime, 250), //
   words: 'apple,linux,javascript,tutorial,legend,man,woman'.split(','),
   btns: [],
   count: 0,
   correction: [],
+
   // add btns
   addBtns() {
     for (let i = 0; i < str.length; i++) {
@@ -35,7 +40,10 @@ const game = {
       this.count++;
       this.correction.push('O');
       correct.textContent = this.correction.join('');
-      this.finishGame();
+      if (this.count === 3) {
+        this.finishGame();
+        clearInterval(this.clock);
+      }
     }
   },
   copyBtnText() {
@@ -44,9 +52,7 @@ const game = {
     }
   },
   finishGame() {
-    if (game.count === 3) {
-      correct.textContent = 'Thank you for playing';
-    }
+    correct.textContent = 'Thank you for playing';
   },
 };
 
@@ -82,6 +88,13 @@ const shuffle = () => {
     rShift();
   }
 };
+
+//현재 경과 시간을 알려주는 함수
+function currentTime() {
+  const seconds = new Date();
+  const currentT = (seconds - game.startTime) / 1000;
+  progress.textContent = currentT;
+}
 
 function init() {
   game.choice();
